3f7b442670ffadff11a1eafd73c580b498dffd98
asil <asil9802mum@gmail.com>
2024-09-23 16:12:15 +0200

    ex3: implemented basic form validation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,41 +4,72 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        val editText = findViewById<EditText>(R.id.editText)
-        val button = findViewById<Button>(R.id.button)
-        val textView = findViewById<TextView>(R.id.textView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        button.setOnClickListener {
-            val typedText = editText.text.toString()
-            textView.text = typedText
+        binding.button.setOnClickListener {
+            val typedText = "${binding.editText.text} ${binding.editText2.text} ${binding.editText3.text} ${binding.editText4.text} ${binding.editText5.text}"
+            binding.textView.text = typedText
         }
 
-//        setContent {
-//            MyApplicationTheme {
-//                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
-//        }
+
+        binding.button.setOnClickListener {
+            if (validateInputs().isEmpty()) {
+                val typedText = gatherInput()
+                displayResult(typedText)
+            } else {
+                Toast.makeText(this, "Please fill in all fields\n" + validateInputs(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
+
+    private fun gatherInput(): String {
+        return "${binding.editText.text} ${binding.editText2.text} ${binding.editText3.text} ${binding.editText4.text} ${binding.editText5.text}"
+    }
+
+    private fun displayResult(result: String) {
+        binding.textView.text = result
+    }
+
+    private fun validateInputs(): String {
+        if (binding.editText.text.isBlank()) {
+            return "please enter info into field: " + binding.editText.hint.toString()
+        }
+        if (binding.editText2.text.isBlank()) {
+            return "please enter info into field: " + binding.editText2.hint.toString()
+        }
+        if (binding.editText3.text.isBlank()) {
+            return "please enter info into field: " + binding.editText3.hint.toString()
+        }
+        if (binding.editText4.text.isBlank()) {
+            return "please enter info into field: " + binding.editText4.hint.toString()
+        }
+        if (binding.editText5.text.isBlank()) {
+            return "please enter info into field: " + binding.editText5.hint.toString()
+        }
+        return "";
+    }
+
+
 }
 
 
